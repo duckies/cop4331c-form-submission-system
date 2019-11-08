5be632870abff2d3450c11290a9e49785a07b89d
Duckies <gilbert.johnt@gmail.com>
2019-11-08 17:41:50 -0500

    Database class no longer necessary.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,18 +5,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
-import { TypeOrmConfigService } from './typeorm.service';
-import { LoggingService } from './logging/logging.service';
 import { LoggingModule } from './logging/logging.module';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
 @Module({
   imports: [
-    LoggingService,
+    LoggingModule,
     ConfigModule,
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule, LoggingModule],
-      useClass: TypeOrmConfigService,
-      inject: [ConfigService, LoggingService],
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) =>
+        ({
+          type: config.get('DATABASE_TYPE'),
+          host: config.get('DATABASE_HOST'),
+          port: config.getNumber('DATABASE_PORT'),
+          username: config.get('DATABASE_USERNAME'),
+          password: config.get('DATABASE_PASSWORD'),
+          database: config.get('DATABASE_NAME'),
+          ssl: config.getBoolean('DATABASE_SSL'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: config.getBoolean('DATABASE_SYNCHRONIZE'),
+        } as PostgresConnectionOptions | MysqlConnectionOptions),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
