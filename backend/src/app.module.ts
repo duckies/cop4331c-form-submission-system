647cf0e02de27b8bab00122f4c93ffeb0d962ce9
Duckies <gilbert.johnt@gmail.com>
2019-11-07 22:44:10 -0500

    Database, logging, and configuration scaffolding.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { TypeOrmConfigService } from './typeorm.service';
+import { LoggingService } from './logging/logging.service';
+import { LoggingModule } from './logging/logging.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    LoggingService,
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule, LoggingModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService, LoggingService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
