f9df3dc0c46cbbc33353a6ea66472ae2b16c4b81
Duckies <gilbert.johnt@gmail.com>
2019-11-09 00:58:27 -0500

    Explain what the Usr() decorator does.

--- backend/src/user/user.decorator.ts
+++ backend/src/user/user.decorator.ts
@@ -1,5 +1,9 @@
 import { createParamDecorator } from '@nestjs/common';
 
+/**
+ * Allows you to use "@Usr() user: User" in controller params
+ * for retrieving the user on authenticated requests.
+ */
 export const Usr = createParamDecorator((data, req) => {
   return req.user;
 });
