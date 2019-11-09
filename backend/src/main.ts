52b1c51cf975d8f2097367e1a5bae2ca5b8e9ffb
Duckies <gilbert.johnt@gmail.com>
2019-11-09 18:05:46 -0500

    Enable the validation pipe.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Whitelist all arguments so they must be described in a DTO.
+   * ForbidNonWhitelisted to not allow requests with extraneous information.
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
